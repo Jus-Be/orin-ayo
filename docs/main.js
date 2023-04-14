bc2896d2dc11b614d73e90fe79c11d2aa10ff025
Dele Olajide <dele@4ng.net>
2023-04-15 00:53:24 +0100

    wip

--- docs/main.js
+++ docs/main.js
@@ -723,7 +723,33 @@ function transposeNote(root) {
 		
 		else if (root == 0x23) root = 0x36;	
 		else if (root == 0x26) root = 0x32;
-		else if (root == 0x27) root = 0x33;					
+		else if (root == 0x27) root = 0x33;			
+	else
+	if (keyChange == 7) {
+		if (root == 0x31) root = 0x35;
+		else if (root == 0x32) root = 0x36;		
+		else if (root == 0x33) root = 0x37;
+		else if (root == 0x34) root = 0x41;	
+		else if (root == 0x35) root = 0x32;
+		else if (root == 0x36) root = 0x33;	
+		else if (root == 0x37) root = 0x44;
+		
+		else if (root == 0x23) root = 0x46;	
+		else if (root == 0x26) root = 0x42;
+		else if (root == 0x27) root = 0x34;				
+	}	else
+	if (keyChange == 8) {
+		if (root == 0x31) root = 0x45;
+		else if (root == 0x32) root = 0x46;		
+		else if (root == 0x33) root = 0x31;
+		else if (root == 0x34) root = 0x32;	
+		else if (root == 0x35) root = 0x42;
+		else if (root == 0x36) root = 0x34;	
+		else if (root == 0x37) root = 0x35;
+		
+		else if (root == 0x23) root = 0x31;	
+		else if (root == 0x26) root = 0x33;
+		else if (root == 0x27) root = 0x44;				
 	}	
 	return root;
 }
