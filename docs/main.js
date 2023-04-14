47d0691db086d69e6e6059ef2db9c56654b62cef
Dele Olajide <dele@4ng.net>
2023-04-15 00:42:11 +0100

    typo fix

--- docs/main.js
+++ docs/main.js
@@ -682,7 +682,7 @@ function transposeNote(root) {
 		else if (root == 0x23) root = 0x44;	
 		else if (root == 0x26) root = 0x37;
 		else if (root == 0x27) root = 0x41;					
-	}	
+	}
 	else
 	if (keyChange == 4) {
 		if (root == 0x31) root = 0x33;
@@ -696,6 +696,34 @@ function transposeNote(root) {
 		else if (root == 0x23) root = 0x35;	
 		else if (root == 0x26) root = 0x31;
 		else if (root == 0x27) root = 0x32;					
+	}	
+	else
+	if (keyChange == 5) {
+		if (root == 0x31) root = 0x34;
+		else if (root == 0x32) root = 0x35;		
+		else if (root == 0x33) root = 0x36;
+		else if (root == 0x34) root = 0x46;	
+		else if (root == 0x35) root = 0x31;
+		else if (root == 0x36) root = 0x32;	
+		else if (root == 0x37) root = 0x33;
+		
+		else if (root == 0x23) root = 0x45;	
+		else if (root == 0x26) root = 0x41;
+		else if (root == 0x27) root = 0x42;					
+	}	
+	else
+	if (keyChange == 6) {
+		if (root == 0x31) root = 0x44;
+		else if (root == 0x32) root = 0x45;		
+		else if (root == 0x33) root = 0x46;
+		else if (root == 0x34) root = 0x31;	
+		else if (root == 0x35) root = 0x41;
+		else if (root == 0x36) root = 0x42;	
+		else if (root == 0x37) root = 0x34;
+		
+		else if (root == 0x23) root = 0x36;	
+		else if (root == 0x26) root = 0x32;
+		else if (root == 0x27) root = 0x33;					
 	}	
 	return root;
 }
