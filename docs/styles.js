1e3a1a1200f4d9a3eac48222cfaeedc1446f8d36
Dele Olajide <dele@4ng.net>
2023-04-28 16:15:50 +0100

    analog balld style

--- docs/styles.js
+++ docs/styles.js
@@ -1,5 +1,272 @@
 var drum_loops = [
 {
+	name: "analog_ballad",
+	label: "Analog Ballad",
+	bpm: 85,
+	drums: {
+		url: "assets/drums/analog_ballad.ogg",
+		duration: 69810,
+		int1: {
+			start: 0,
+			stop: 2862
+		},
+		arra: {
+			start: 2862,
+			stop: 14154
+		},	
+		arrb: {
+			start: 14154,
+			stop:  25449
+		},	
+		arrc: {
+			start: 25449,
+			stop:  36744
+		},	
+		arrd: {
+			start: 36744,
+			stop:  59334
+		},	
+		end1: {
+			start: 59334,
+			stop:  69810
+		},
+		brka: {
+			start: 11330,
+			stop:  14154
+		},	
+		brkb: {
+			start: 11330,
+			stop:  14154
+		},	
+		brkc: {
+			start: 11330,
+			stop:  14154
+		},	
+		brkd: {
+			start: 11330,
+			stop:  14154
+		},	
+		fila: {
+			start: 11330,
+			stop:  14154
+		},	
+		filb: {
+			start: 11330,
+			stop:  14154
+		},	
+		filc: {
+			start: 11330,
+			stop:  14154
+		},	
+		fild: {
+			start: 11330,
+			stop:  14154
+		}			
+	},
+/*	
+	bass: {
+		url: "assets/bass/analog_ballad.ogg",
+		duration: 135430,
+		key0: {
+			start: 5660,
+			stop: 8485
+		},
+		key1: {
+			start: 16980,
+			stop:  19805
+		},	
+		key2: {
+			start: 28300,
+			stop:  31125
+		},	
+		key3: {
+			start: 39545,
+			stop:  42370
+		},	
+		key4: {
+			start: 25600,
+			stop:  32000
+		},	
+		key5: {
+			start: 32000,
+			stop:  38400
+		},
+		key6: {
+			start: 38400,
+			stop:  44800
+		},
+		key7: {
+			start: 44800,
+			stop:  51200
+		},	
+		key8: {
+			start: 51200,
+			stop:  57600
+		},	
+		key9: {
+			start: 57600,
+			stop:  64000
+		},	
+		key10: {
+			start: 64000,
+			stop:  70400
+		},	
+		key11: {
+			start: 70400,
+			stop:  76800
+		}		
+	},	
+*/	
+	chords: {
+		url: "assets/chords/analog_ballad.ogg",
+		duration: 135552,
+		key0: {
+			start: 0,
+			stop: 11296
+		},
+		key1: {
+			start: 11296,
+			stop: 22592
+		},	
+		key2: {
+			start: 22592,
+			stop:  33888
+		},	
+		key3: {
+			start: 33888,
+			stop:  45184
+		},	
+		key4: {
+			start: 45184,
+			stop:  56480
+		},	
+		key5: {
+			start: 56480,
+			stop:  67776
+		},
+		key6: {
+			start: 67776,
+			stop:  79072
+		},
+		key7: {
+			start: 79072,
+			stop:  90368
+		},	
+		key8: {
+			start: 90368,
+			stop:  101664
+		},	
+		key9: {
+			start: 101664,
+			stop:  112960
+		},	
+		key10: {
+			start: 112960,
+			stop:  124256
+		},	
+		key11: {
+			start: 124256,
+			stop:  135552
+		},
+		key0_min: {
+			start: 128000,
+			stop:  138667
+		},
+		key1_min: {
+			start: 138667,
+			stop:  149333
+		},	
+		key2_min: {
+			start: 149333,
+			stop:  160000
+		},	
+		key3_min: {
+			start: 160000,
+			stop:  170667
+		},	
+		key4_min: {
+			start: 170667,
+			stop:  181333
+		},	
+		key5_min: {
+			start: 181333,
+			stop:  192000
+		},
+		key6_min: {
+			start: 192000,
+			stop:  202667
+		},
+		key7_min: {
+			start: 202667,
+			stop:  213333
+		},	
+		key8_min: {
+			start: 213333,
+			stop:  224000 
+		},	
+		key9_min: {
+			start: 224000,
+			stop:  234667
+		},	
+		key10_min: {
+			start: 234667,
+			stop:  245333
+		},	
+		key11_min: {
+			start: 245333,
+			stop:  256000
+		},
+		key0_sus: {
+			start: 256000,
+			stop:  261333
+		},
+		key1_sus: {
+			start: 261333,
+			stop:  266666
+		},	
+		key2_sus: {
+			start: 266666,
+			stop:  271999
+		},	
+		key3_sus: {
+			start: 271999,
+			stop:  277332
+		},	
+		key4_sus: {
+			start: 277332,
+			stop:  282665
+		},	
+		key5_sus: {
+			start: 282665,
+			stop:  287998
+		},
+		key6_sus: {
+			start: 287998,
+			stop:  293331
+		},
+		key7_sus: {
+			start: 293331,
+			stop:  298664
+		},	
+		key8_sus: {
+			start: 298664,
+			stop:  303998 
+		},	
+		key9_sus: {
+			start: 303998,
+			stop:  309332
+		},	
+		key10_sus: {
+			start: 309332,
+			stop:  314666
+		},	
+		key11_sus: {
+			start: 314666,
+			stop:  320000
+		}		
+	}		
+},
+{
 	name: "8beat",
 	label: "8 Beat Standard",
 	bpm: 100,
