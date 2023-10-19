cd5cf4c5cc21143ad27636db0d5f4a47ea9dcc7d
Dele Olajide <dele@4ng.net>
2023-10-19 21:57:50 +0100

    aeros wip

--- docs/js/main.js
+++ docs/js/main.js
@@ -790,11 +790,11 @@ async function setupUI(config,err) {
 					
 			if (e?.controller.number == 113) 
 			{					
-				if (e.value == 0) {
+				if (e.value == 71 || e.value == 73 || e.value == 81 || e.value == 82 || || e.value == 91) {
 					console.debug("Aeros section change message", aerosChordTrack);			  
 					output.sendControlChange (39, aerosChordTrack, 4); 	// play current chord on new part					
 					
-					if (aerosAux) {	
+					if (e.value == 73) {	
 						aerosAux = false;
 						aerosAuxMode = true;
 						// switch to aux part
