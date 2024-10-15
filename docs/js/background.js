99cfed06c211787bbf1676697e4acc3d77eee75e
Dele Olajide <dele@4ng.net>
2024-10-15 17:47:10 +0100

    adjust width

--- docs/js/background.js
+++ docs/js/background.js
@@ -88,7 +88,7 @@ const createOrinAyoWindow = () => {
 	
 	chrome.windows.create(data, (win) => {
 		chrome.storage.local.set({orinAyoWin: win.id});			
-		chrome.windows.update(win.id, {width: 1150, height: 1040});
+		chrome.windows.update(win.id, {width: 1050, height: 1040});
 	});
 }
 
