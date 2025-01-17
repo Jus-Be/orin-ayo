4173f13c542f1b6abefd81ff2b6998e4f85be16a
Dele Olajide <dele@4ng.net>
2025-01-17 11:10:52 +0000

    test mobile view

--- docs/js/main.js
+++ docs/js/main.js
@@ -25,6 +25,8 @@ const WHAMMY = 2;
 const LOGO = 12;
 const CONTROL = 100;
 
+var desktopContainer = null;
+var mobileContainer = null;
 var midiInstrCheckedEle = [];
 var midiVolumeEle = [];
 var programChangeEle = null;
@@ -1742,11 +1744,14 @@ function initLavaGenie() {
 
 async function onloadHandler() {
 	console.debug("onloadHandler");
-	
-	let version = "latest";
+		
+	let version = "1.0.0";
 	if (!!chrome.runtime?.getManifest) version = chrome.runtime.getManifest().version;
 	document.title = "Orin Ayo | " + version;
   
+	desktopContainer = document.getElementById("desktop");
+	mobileContainer = document.getElementById("mobile");
+	
 	playButton = document.querySelector("#play");
 	gamePadModeButton = document.querySelector("#gamepad_mode");
 	styleType = document.querySelector("#style_type");
@@ -1765,6 +1770,31 @@ async function onloadHandler() {
 	programChangeEle = document.querySelector("#program-change");
 	autoFillCheckedEle = document.querySelector("#autoFill");
 	introEndCheckedEle = document.querySelector("#introEnd");		
+	loadFile = document.querySelector("#load_file")
+	
+	const saveReg = document.querySelector("#save_reg")
+	
+
+	if (mobileCheck()) {
+		desktopContainer.style.display = "none";	
+		
+		mobileContainer.style.display = "";
+		const mobileToolbar = document.getElementById("mobile-toolbar");
+		mobileToolbar.append(playButton);
+		mobileToolbar.append(loadFile);
+		mobileToolbar.append(saveReg);
+		
+		const drumKnob = createKnob("drum-volume", 50, 0, 100, '#88ff88');
+		const bassKnob = createKnob("bass-volume", 50, 0, 100, '#ff8888');
+		const chordKnob = createKnob("chord-volume", 50, 0, 100, '#8888ff');
+		const leadKnob = createKnob("lead-volume", 50, 0, 100, '#888888');		
+			
+
+	} else {
+		mobileContainer.style.display = "none";
+		desktopContainer.style.display = "";			
+	}
+	
 	
 	document.body.addEventListener('click', function(event) 	{
 		// TODO
@@ -1930,8 +1960,6 @@ async function onloadHandler() {
 	chordaBluetooth.addEventListener('click', function(event) {
 		onChordaConnect(event);
 	});		
-
-	const saveReg = document.querySelector("#save_reg")
 		
 	saveReg.addEventListener('click', function(event) {
 		const slot = prompt("Enter save slot number");
@@ -1947,8 +1975,6 @@ async function onloadHandler() {
 		registration = 0;
 		location.reload();
 	});	
-	
-	loadFile = document.querySelector("#load_file")
 		
 	loadFile.addEventListener('click', function(event) {
 		upload.click();	
@@ -8265,6 +8291,25 @@ function arraysEqual(a, b) {
   return true;
 }
 
+function mobileCheck() {
+  let check = false;
+  (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+  return check;
+};
+
+function createKnob(id, value, valMin, valMax, color) {
+	const knob = pureknob.createKnob(200, 200);
+
+	knob.setProperty('angleStart', -0.75 * Math.PI);
+	knob.setProperty('angleEnd', 0.75 * Math.PI);
+	knob.setProperty('colorFG', color);
+	knob.setProperty('trackWidth', 0.4);
+	knob.setProperty('valMin', valMin);
+	knob.setProperty('valMax', valMax);
+
+	knob.setValue(value);
+	document.getElementById(id).appendChild(knob.node());							
+}
 // -------------------------------------------------------
 //
 //  X-Touch MIDI Controller Device
